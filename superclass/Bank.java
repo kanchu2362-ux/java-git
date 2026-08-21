49dfe228ac1a8b81ebe98a298c14670460bf48b5
kanchana T.S <kanchu2362@gmail.com>
2026-08-21 08:47:28 +0530

    super method

---
+++ superclass/Bank.java
@@ -0,0 +1,34 @@
+package java-git.superclass;
+
+public class Bank
+{
+    void display()
+    {
+        System.out.println("Bank provides financial services");
+    }
+}
+
+class Account extends Bank
+{
+    void display()
+    {
+        System.out.println("Account is created");
+    }
+
+    void show()
+    {
+        super.display();
+        display();
+    }
+}
+
+class TestSuper2
+{
+    public static void main(String args[])
+    {
+        Account a = new Account();
+        a.show();
+    }
+} {
+    
+}
